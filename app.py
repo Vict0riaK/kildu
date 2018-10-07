028dd1bc6b6fe3d20ebcb2335d8f5b4e0c05cb15
victoria <victoria.kildyushevskaya@gmail.com>
2018-10-07 19:31:29 +0200

    first change

--- app.py
+++ app.py
@@ -24,13 +24,17 @@ def graph():
 #    if request.method == 'POST':
         app.vars['ticker'] = request.form['ticker']
         
-        api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=gVz7XbzeecyxHdkCn8yB' % app.vars['ticker']
+        # api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=gVz7XbzeecyxHdkCn8yB' % app.vars['ticker']
+
+
+        api_url = 'https://www.quandl.com/api/v3/datasets/ZILLOW/C25709_ZRISFRR.json?api_key=QT-coVZNkYPJCs6R9Tkj'
         session = requests.Session()
         session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
         raw_data = session.get(api_url)
 
         a = raw_data.json()
-        df = pandas.DataFrame(a['data'], columns=a['column_names'])
+        a1 = a['dataset']
+        df = pandas.DataFrame(a1['data'], columns=a1['column_names'])
 
         df['Date'] = pandas.to_datetime(df['Date'])
 
@@ -39,13 +43,16 @@ def graph():
             x_axis_type='datetime')
         
         if request.form.get('Close'):
-            p.line(x=df['Date'].values, y=df['Close'].values,line_width=2, legend='Close')
+            p.line(x=df['Date'].values, y=df['Value'].values, line_width=2, legend='Close')
+            # p.line(x=df['Date'].values, y=df['Close'].values,line_width=2, legend='Close')
         if request.form.get('Adj. Close'):
             p.line(x=df['Date'].values, y=df['Adj. Close'].values,line_width=2, line_color="green", legend='Adj. Close')
         if request.form.get('Open'):
             p.line(x=df['Date'].values, y=df['Open'].values,line_width=2, line_color="red", legend='Open')
         if request.form.get('Adj. Open'):
             p.line(x=df['Date'].values, y=df['Adj. Open'].values,line_width=2, line_color="purple", legend='Adj. Open')
+
+
         script, div = components(p)
         return render_template('graph.html', script=script, div=div)
 
